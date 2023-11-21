5e220165527f0c0f2c75512b7547691869f8b107
Kelsey Hughes <kelbri10@gmail.com>
2023-11-21 13:52:43 -0500

    added animations to project pages

--- client/src/pages/Work/Projects/Bookhood.tsx
+++ client/src/pages/Work/Projects/Bookhood.tsx
@@ -1,32 +1,59 @@
 import "../../styles/Project.css"; 
 import "../../../main.css"
+import { motion } from "framer-motion";
+
 const Bookhood = () => { 
+    
+    
     return( 
         <div className="container">
             <header>
-                <div className="gradient_box"></div>
+               
+                <motion.div
+                initial={{opacity: 0, x: -100}}
+                animate={{opacity: 1, x:0}}
+                transition={{delay: 0.25, duration: 0.5}}>
+                    <div className="gradient_box"></div>
+                </motion.div>
 
-                <div className="project_header">
-                    <div>
-                        <h1>Bookhood</h1>
-                        <h2>Book tracking made easy</h2>
+            
+                <motion.div
+                initial={{opacity: 0, x: 100}}
+                animate={{opacity: 1, x:0}}
+                transition={{delay: 0.35, duration: 0.5}}>
+                    <div className="project_header">
+                        <div>
+                            <h1>Bookhood</h1>
+                            <h2>Book tracking made easy</h2>
+                        </div>
+
+                        <div className="project_links">
+                            <a href="https://github.com/kelbri10/bookhood" target="_blank">github</a> 
+
+                            <a href="google.com" target="_blank">live site</a>
+                        </div>
                     </div>
-
-                    <div className="project_links">
-                        <a href="https://github.com/kelbri10/bookhood" target="_blank">github</a> 
-
-                        <a href="google.com" target="_blank">live site</a>
-                    </div>
-                </div>
+                </motion.div>
             </header>
-
+            
             <section className="project_description">
-                <article className="project_info">
-                    <h3>What is Bookhood?</h3>
-                    <p>
-                          Bookhood is a simple, easy-to-use book tracking application. It allows users to save completed and in-progress books to their library. The application also allows users to edit their book's information to make sure they have the most up-to-date ratings and titles listed. Returning users are able to login with their password and email, while new users are able to create an account and be redirected to the library where they can add books using the form. Bookhood was inspired by my own personal reading journey and desire for a straight-forward application to track my own books without becoming overwhelmed.
-                    </p>
-                </article>
+                {/* Come in from the bottom staggered */}
+                <motion.div
+                initial={{opacity: 0, y: 50}}
+                animate={{opacity: 1, y:0}}
+                transition={{delay: 0.45, duration: 0.5}}>
+                    <article className="project_info">
+                        <h3>What is Bookhood?</h3>
+                        <p>
+                            Bookhood is a simple, easy-to-use book tracking application. It allows users to save completed and in-progress books to their library. The application also allows users to edit their book's information to make sure they have the most up-to-date ratings and titles listed. Returning users are able to login with their password and email, while new users are able to create an account and be redirected to the library where they can add books using the form. Bookhood was inspired by my own personal reading journey and desire for a straight-forward application to track my own books without becoming overwhelmed.
+                        </p>
+                    </article>
+                </motion.div>
+
+                <motion.div
+                initial={{opacity: 0, y: 50}}
+                animate={{opacity: 1, y:0}}
+                transition={{delay: 0.55, duration: 0.5}}>
                 <article className="project_info">
                     <h3>What technologies did I use?</h3>
 
@@ -38,16 +65,23 @@ const Bookhood = () => {
                         To block unauthorized user's from entering the site, I wrote a security rule that checked whether the auth was null, if so, they are directed to login. If an unauthenticated user tries to access the library without being signed in, they are shown an error page that links back to the login page as well. To store user data and books, I used Firestore database. This allowed me to create a collection of users which stored each user's uid as a document. Within each user document is a books array that stores the saved author, title, rating, start date and completed date. 
                     </p>
                 </article>
-                <article className="project_info">
-                    <h3>Conclusions</h3>
-                    <p>
-                        This project helped me to become more comfortable navigating the Firebase authentication and Firestore database documentation. For future projects, I definitely would want to destructure the data from the user further. Instead of using an array and iterating over that array by a unique id, I would instead create another collection and save each book as an individual document. This would make the app more stable long term if a user has, for example, 100+ plus books. Instead of having to iterate linearly over an array, I would be able to better maximize the Firestore database features and ability to find pieces of data when the size of the pool is large. 
-                    </p>
-                    <br></br>
-                    <p>
-                        Other features I would like to add in the future would be book suggestions and to add friends and view their books as well. I think a social aspect to this project would encourage users to return more frequently and increase engagement.
-                    </p>
-                </article>
+                </motion.div>
+
+                <motion.div
+                initial={{opacity: 0, y: 50}}
+                animate={{opacity: 1, y:0}}
+                transition={{delay: 0.65, duration: 0.5}}>
+                    <article className="project_info">
+                        <h3>Conclusions</h3>
+                        <p>
+                            This project helped me to become more comfortable navigating the Firebase authentication and Firestore database documentation. For future projects, I definitely would want to destructure the data from the user further. Instead of using an array and iterating over that array by a unique id, I would instead create another collection and save each book as an individual document. This would make the app more stable long term if a user has, for example, 100+ plus books. Instead of having to iterate linearly over an array, I would be able to better maximize the Firestore database features and ability to find pieces of data when the size of the pool is large. 
+                        </p>
+                        <br></br>
+                        <p>
+                            Other features I would like to add in the future would be book suggestions and to add friends and view their books as well. I think a social aspect to this project would encourage users to return more frequently and increase engagement.
+                        </p>
+                    </article>
+                </motion.div>
             </section>
         </div>
     )
--- client/src/pages/Work/Projects/TheKeyShop.tsx
+++ client/src/pages/Work/Projects/TheKeyShop.tsx
@@ -1,53 +1,82 @@
 import "../../styles/Project.css"; 
 import "../../../main.css"
+import { motion } from "framer-motion";
+
 const TheKeyShop = () => { 
     return( 
         <div className="container">
             <header>
-                <div className="gradient_box"></div>
+                <motion.div
+                initial={{opacity: 0, x: -100}}
+                animate={{opacity: 1, x:0}}
+                transition={{delay: 0.25, duration: 0.5}}>
+                    <div className="gradient_box"></div>
+                </motion.div>
 
-                <div className="project_header">
-                    <div>
-                        <h1>The Key Shop</h1>
-                        <h2>A Keyboard Enthusiast's Dream Shop</h2>
-                    </div>
+                <motion.div
+                initial={{opacity: 0, x: 100}}
+                animate={{opacity: 1, x:0}}
+                transition={{delay: 0.35, duration: 0.5}}>
+                    <div className="project_header">
+                        <div>
+                            <h1>The Key Shop</h1>
+                            <h2>A Keyboard Enthusiast's Dream Shop</h2>
+                        </div>
 
-                    <div className="project_links">
-                        <a href="https://github.com/kelbri10/The-Key-Shop" target="_blank">github</a> 
-                        
-                        <a href="https://thekeyshop.onrender.com/" target="_blank">live site</a>
+                        <div className="project_links">
+                            <a href="https://github.com/kelbri10/The-Key-Shop" target="_blank">github</a> 
+                            
+                            <a href="https://thekeyshop.onrender.com/" target="_blank">live site</a>
+                        </div>
                     </div>
-                </div>
+                </motion.div>
             </header>
 
             <section className="project_description">
-                <article className="project_info">
-                    <h3>What is The Key Shop?</h3>
-                    <p>
-                    Inspiration for The Key Shop came from my love customizable keyboards. There are a lot of great boards and customizable options available currently with some of the well known companies being Cannonkeys, Novelkeys, and Osume. The Key Shop is a mock E-commerce website that mimics a standard online website to shop for switches, boards, and keycaps.
-                    </p>
-                </article>
-                <article className="project_info">
-                    <h3>What technologies did I use?</h3>
+                <motion.div
+                initial={{opacity: 0, y: 50}}
+                animate={{opacity: 1, y:0}}
+                transition={{delay: 0.45, duration: 0.5}}>
+                    <article className="project_info">
+                        <h3>What is The Key Shop?</h3>
+                        <p>
+                        Inspiration for The Key Shop came from my love customizable keyboards. There are a lot of great boards and customizable options available currently with some of the well known companies being Cannonkeys, Novelkeys, and Osume. The Key Shop is a mock E-commerce website that mimics a standard online website to shop for switches, boards, and keycaps.
+                        </p>
+                    </article>
+                </motion.div>
 
-                    <p>
-                        The backend was built using Node for the server, and MongoDB as the database. ExpressJS helped me to setup my api calls and I used Postman to test my get requests to the database I setup. Placeholder images were used because the main focus of the project was the functionality and userflow from arriving to the site via the landing page up until the cart function.
-                    </p>
-                    <br></br>
-                    <p>
-                        In order to track what the customer had in their cart, I created a Cart Context using the Context api. This allowed me to share an items array across the pages, so that when the customer is ready to checkout and views their cart, it is up-to-date with their shopping. I used React Router to link the pages to one another and TailwindCSS was used to efficiently style the site's pages. 
-                    </p>
-                </article>
-                <article className="project_info">
-                    <h3>Conclusions</h3>
-                    <p>
-                        Creating my own E-commerce site helped me to better understand some of the programming logic that works under the hood of sites such as Shopify, Etsy, and other similar sites that allow business owners to create their own online shops. For a future iteration onto this project, I would create an admin sign-in that would allow  for the shop owner to add, delete, and edit their store's information and allow for the product's inventory data be updated on MongoDB. 
-                    </p>
-                    <br></br>
-                    <p>
-                        Another feature I would like to add in the future would be allowing customers to leave anonymous or signed reviews underneath products. This would allow for another level of social interaction and customer feedback.
-                    </p>
-                </article>
+                <motion.div
+                initial={{opacity: 0, y: 50}}
+                animate={{opacity: 1, y:0}}
+                transition={{delay: 0.55, duration: 0.5}}>
+                    <article className="project_info">
+                        <h3>What technologies did I use?</h3>
+
+                        <p>
+                            The backend was built using Node for the server, and MongoDB as the database. ExpressJS helped me to setup my api calls and I used Postman to test my get requests to the database I setup. Placeholder images were used because the main focus of the project was the functionality and userflow from arriving to the site via the landing page up until the cart function.
+                        </p>
+                        <br></br>
+                        <p>
+                            In order to track what the customer had in their cart, I created a Cart Context using the Context api. This allowed me to share an items array across the pages, so that when the customer is ready to checkout and views their cart, it is up-to-date with their shopping. I used React Router to link the pages to one another and TailwindCSS was used to efficiently style the site's pages. 
+                        </p>
+                    </article>
+                </motion.div>
+
+                <motion.div
+                initial={{opacity: 0, y: 50}}
+                animate={{opacity: 1, y:0}}
+                transition={{delay: 0.65, duration: 0.5}}>
+                    <article className="project_info">
+                        <h3>Conclusions</h3>
+                        <p>
+                            Creating my own E-commerce site helped me to better understand some of the programming logic that works under the hood of sites such as Shopify, Etsy, and other similar sites that allow business owners to create their own online shops. For a future iteration onto this project, I would create an admin sign-in that would allow  for the shop owner to add, delete, and edit their store's information and allow for the product's inventory data be updated on MongoDB. 
+                        </p>
+                        <br></br>
+                        <p>
+                            Another feature I would like to add in the future would be allowing customers to leave anonymous or signed reviews underneath products. This would allow for another level of social interaction and customer feedback.
+                        </p>
+                    </article>
+                </motion.div>
             </section>
         </div>
     )
